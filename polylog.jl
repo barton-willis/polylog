11a462157d0eb18326d52b804c3467dff12b4bdd
Barton Willis <willisb@unk.edu>
2024-01-26 15:45:04 -0600

    Update polylog.jl

--- polylog.jl
+++ polylog.jl
@@ -1,7 +1,10 @@
-# Barton Willis, Copyright 2019
+# Barton Willis, Copyright 2019, 2024
 
-# This work is licensed under a Creative Commons Attribution 4.0 International License.
-# Julia code for the evaluation of the hypergeometric function 2F1.
+# This work is licensed under the CC0 1.0 Universal license
+# Julia code for the evaluation of the dilogarithm. The method is
+# based on "The binomial transform of p-recursive sequences and 
+# dilogarithm function," Stephanie Harshbarger and Barton Willis.
+# https://arxiv.org/pdf/1910.06928.pdf
 
 # Extend eps.
 import Base.eps
@@ -20,9 +23,33 @@ eps(x::Type{Complex{Float64}}) = eps(Float64)
 eps(x::Complex{BigFloat}) = eps(BigFloat)
 eps(x::Type{Complex{BigFloat}}) = eps(BigFloat)
 
-#convert complex to a bigfloat
+# convert a complex to a bigfloat
 BigFloat(x::Complex) = BigFloat(real(x)) + BigFloat(imag(x))im
 
+"""
+    polylog2(x)
+
+Compute the numeric value of the dilogarithm. For a definition of this function,
+see http://dlmf.nist.gov/25.12.E1 . The function returns a four tuple of the 
+for (dilogarithm(x), cnd, n, bool), where cnd is the condition sum that is used
+for evalution, n is the number of terms summed, and bool is a boolean that 
+indicates sucess or failure.
+
+```
+julia> polylog2(1.01)
+julia> (1.700732144324037 - 0.03125988630910074im, 2.2149194678541363e-18, 9, true)
+
+The input can be a complex number:
+julia> polylog2(3.1 + 5.1*im)
+julia> -1.0831276752057937 + 3.9312546561253905im, 1.8745409421641466e-17 + 3.595690852077723e-17im, 17, true)
+
+Finally, the input can be a BigFloat:
+
+julia> polylog2(BigFloat(1.01))
+julia> (1.700732144324037086378889112827585843561092568405653427462945581660835230870607 - 0.03125988630910073633439461464037805326512443369418564280068429886030330779614308im, 1.722934710961044873101367377667284112099228890489560282524331651371580237533837e-79, 36, true)
+
+```
+"""
 function polylog2(x::Number)
 	T = typeof(x)
 	cnd = x -> if x == 2 Inf else abs2(x/(2-x)) end
@@ -45,8 +72,8 @@ function polylog2(x::Number)
 	end
 end
 	
-# return value of polylog(2,x), the condition number of the sum, the number of terms summed, and a
-# boolean that indicates sucess or failure.
+# return value of polylog(2,x), the condition number of the sum, the 
+# number of terms summed, and a boolean that indicates sucess or failure.
 function polylog2_helper(x::Number)
 	T = typeof(x)
 	q0 = x/(1-x/2)
