ab6dff9c26d2978d8ff50ee097e668c9b9f9390d
Barton Willis <willisb@unk.edu>
2024-01-27 09:36:50 -0600

    more test code

--- polylog_test.jl
+++ polylog_test.jl
@@ -17,8 +17,22 @@ function dlmf_25_12_E5(z,m::Int64)
          s += polylog2(z*cis(2*pi*k/m))
          k += 1
       end
-      isapprox(polylog2(z^m), m*s, rtol = 16*eps(z))
+      isapprox(polylog2(z^m), m*s, atol = 16*eps(z), rtol = 16*eps(z))
    else
       true
    end
+end
+
+#http://dlmf.nist.gov/25.12.E7
+function dlmf_25_12_E7(x)
+   if 0 <= x && x <= 2*pi
+     isapprox(real(polylog2(cis(x))), pi^2/6 - pi*x/2 + x^2/4,atol= 65*eps(x), rtol = 16*eps(x))
+   else 
+      true
+   end
+end
+
+#See https://en.wikipedia.org/wiki/Dilogarithm
+function polylog2_id_1(x)
+   isapprox(polylog2(x)+polylog2(-x),polylog2(x^2)/2, atol= 16*eps(x), rtol=eps(x))
 end
