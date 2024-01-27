7483f37b798b0e09378a40100e9c1a2a5e3d2985
Barton Willis <willisb@unk.edu>
2024-01-27 07:32:50 -0600

    Update polylog_test.jl
    Tell the test to pass when the input is invalid

--- polylog_test.jl
+++ polylog_test.jl
@@ -1,5 +1,10 @@
-# Test the identity http://dlmf.nist.gov/25.12.E3
+# Test the identity http://dlmf.nist.gov/25.12.E3 .
+# This identity is valid off [1,infty). For a input in [1,infy),
+# return true.
 function dlmf_25_12_3(x)
-   isapprox(polylog2(x) + polylog2(x/(x-1)), -log(1-x)^2 / 2, rtol = 16*eps(x))
+   if imag(x)==0 && x >= 1
+      true
+   else
+     isapprox(polylog2(x) + polylog2(x/(x-1)), -log(1-x)^2 / 2, rtol = 16*eps(x))
 end
 
