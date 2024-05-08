fbb48296115d8ef59654f71ba9a5fb8e40714776
Barton Willis <willisb@unk.edu>
2024-05-08 05:20:34 -0500

    Change the way the tests compute the accuracy
    o Change the way the tests compute the accuracy
    
    o Update the notebook

--- polylog_test.jl
+++ polylog_test.jl
@@ -1,6 +1,6 @@
 # Many of these tests use function identities that the code uses to 
-# transform the input to gain better convergence. Such tests are not particularly
-# good.
+# transform the input to gain better convergence. Such tests are not
+# particularly good accuracy tests.
 
 # A modified relative difference function. When both |a| >= 1 and |b| >= 1 and
 # a & b are real, return abs(a-b)/(ε * min(|a|, |b|))
@@ -18,34 +18,72 @@ function rd(a::Number,b::Number)
      max(rd(real(a),real(b)), rd(imag(a),imag(b)))
 end
 
+"""
+    boa(a::Real, b::Real)
+
+Calculate the number of bits of agreement between two real numbers `a` and `b`.
+Specifically, when `a` and `b` are both nonzero and have the same sign, return 
+
+     floor(Int64, min(precision(a), -log2(|a-b|/min(|a|,|b|))
+
+When `a` and `b` have opposite signs, return zero; and when either `a` or `b` is
+zero, return  floor(Int64, min(precsion(a), -log2(max(|a|,|b|)))).
+
+Also, when `a` and `b` don't have the same type, we apply `promote` to them.
+
+# Examples
+```julia
+julia> boa(2.0, 2.0+ 1/2^10)
+11
+
+julia> boa(0.0, 0.0)
+53
+
+julia> boa(-5.0, 5.0)
+0
+"""
+function boa(a::Real, b::Real)
+   (a,b) = promote(a,b)
+   m =  precision(a)
+   q =  if iszero(a) && iszero(b) # both a & b are zero
+           m
+         elseif sign(a) != sign(b) 
+           0
+         elseif iszero(a)
+           -log2(abs(b))
+         elseif iszero(b)
+            -log2(abs(a))
+         else
+            -log2(abs((a-b)/min(abs(a),abs(b))))
+         end
+   max(0, floor(Int64, min(m,q)))
+ end
+
+function boa(x::Complex,y::Complex)
+   min(boa(real(x),real(y)),boa(imag(x),imag(y)))
+end
+
 function test_report(results)
-   Q = sort(collect(results), by=x->x[1])
+   Q = sort(collect(results), by=x->x[1], rev=true)
    s = 0
    n = 0
-   w = 0
+   w = typemax(Int64)
    for x in Q 
       n += x[2]
       s += x[1]*x[2]
-      w = max(w,x[1])
+      w = min(w,x[1])
    end
    println("number of tests = ", n)
-   println("average error = ",s/n)
-   println("worst error = ",w)
+   println("average correct bits = ",s/n)
+   println("worst correct bits = ",w)
    Q
 end
 
-# Test the identity http://dlmf.nist.gov/25.12.E3 .
-# This identity is valid off [1,infinity). For an input in 
-# [1,infinity), return zero; otherwise, return a ,modified
-# relative difference.
+# Test the identity http://dlmf.nist.gov/25.12.E3 . This identity is valid 
+# off [1,infinity), but this function does not check this condition.
 function dlmf_25_12_3_E3(x)
-   if iszero(imag(x)) && real(x) >= 1
-      d = 0
-   else
-     #rd(polylog2(x) + polylog2(x/(x-1)), -log(1-x)^2 / 2)
-     d = rd(polylog2(x) + polylog2(x/(x-1)), -log(1-x)^2 / 2)
-    end
-   d
+   #rd(polylog2(x) + polylog2(x/(x-1)), -log(1-x)^2 / 2)
+   boa(polylog2(x) + polylog2(x/(x-1)), -log(1-x)^2 / 2)
 end
 
 # Test dlmf_25_12_3_E3 inside and on the unit circle.
@@ -54,9 +92,11 @@ function polylog2_test1(T::DataType,m::Int64,n::Int64)
    for i = 0 : m
       for j = 0 : n
          x = convert(T,(i/m) * cis(2*pi*j/n))
-         q = dlmf_25_12_3_E3(x)
-         results[q] = if haskey(results,q) results[q]+1 else 1 end
-   end
+         if !iszero(imag(x)) || real(x) < 1
+            q = dlmf_25_12_3_E3(x)
+            results[q] = if haskey(results,q) results[q]+1 else 1 end
+         end
+      end
    end
    test_report(results)
 end
@@ -64,7 +104,7 @@ end
 # Test dlmf_25_12_3_E3 on the unit circle
 function polylog2_test2(T::DataType,n::Int64)
    results = Dict()
-   for j = 0 : n-1
+   for j = 1 : n-1
        x = convert(T, cis(2*pi*j/n))
        q = dlmf_25_12_3_E3(x)
        results[q] = if haskey(results,q) results[q]+1 else 1 end
@@ -83,7 +123,7 @@ function dlmf_25_12_E5(T,z,m::Int64)
          k += 1
       end
       s = KahanSum(s...)
-      rd(polylog2(z^m), m*s)
+      boa(polylog2(z^m), m*s)
    else
       true
    end
@@ -105,7 +145,7 @@ end
 function dlmf_25_12_E7(T,x)
    if zero(typeof(x)) <= x && x <= convert(typeof(x), 2*pi)
      pie = convert(T,pi)
-     rd(real(polylog2(cis(x))),  pie*(pie/6 - x/2) + x^2/4)
+     boa(real(polylog2(cis(x))),  pie*(pie/6 - x/2) + x^2/4)
    else 
       true
    end
@@ -123,7 +163,7 @@ end
 
 #See https://en.wikipedia.org/wiki/Dilogarithm
 function polylog2_id_1(x)
-   rd(polylog2(x)+polylog2(-x),polylog2(x^2)/2)
+   boa(polylog2(x)+polylog2(-x),polylog2(x^2)/2)
 end
 
 function polylog2_test5(T::DataType,n::Int64)
